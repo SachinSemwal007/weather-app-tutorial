1e427c9818f0326164d7f7ca83a59669c84f6111
Sachin Semwal <sachinsemwal18@gmail.com>
2023-08-15 23:51:29 +0530

    Updated the code to handle errors when fetching weather data.

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ let weather = {
     )
       .then((response) => {
         if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
+         // alert("No weather found.");
+          //throw new Error("No weather found.");
         }
         return response.json();
       })
